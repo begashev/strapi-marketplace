eee04556dba7882e53dcedd7a77496d0e3232016
BEGASHEV <smicro165@gmail.com>
2026-06-18 12:55:50 +0500

    12

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "../components/ui/navbar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className="flex min-h-full flex-col bg-white"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        <main className="flex-grow container mx-auto px-4 py-8">{children}</main></body>
     </html>
   );
 }
